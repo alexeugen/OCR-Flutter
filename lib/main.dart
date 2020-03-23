970c9dfd7b94cdc4631c6d0d8fa7f983c7a39f11
unknown <alexeugen88@gmail.com>
2020-03-23 21:31:59 +0200

    back to origins

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/scanner/sanner.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  Scanner createState() => Scanner();
+}
