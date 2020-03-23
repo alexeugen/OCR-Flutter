720e011ed35837e38ac23b99cea15dcb0d6a7e6e
unknown <alexeugen88@gmail.com>
2020-03-23 19:50:09 +0200

    initial setup

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_mobile_vision/FlutterMobileVisionPlugin.h>)
+#import <flutter_mobile_vision/FlutterMobileVisionPlugin.h>
+#else
+@import flutter_mobile_vision;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterMobileVisionPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterMobileVisionPlugin"]];
+}
+
+@end
